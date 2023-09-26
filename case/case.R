eb4fd7e03f328b5707404e8f0ce10bfdf473bbed
Henri Kauhanen <nenahuak@gmail.com>
2023-09-26 17:55:34 +0200

    Updates to case code

--- case/case.R
+++ case/case.R
@@ -32,4 +32,18 @@ print(f)
 
 # proposed facet plot
 df <- props_of_main_functions_grouped
-ggplot(df, aes(x=date, y=prop)) + geom_point(color="#3d62a6") + geom_smooth(se=FALSE, span=1.5, color="black") + facet_wrap(.~func) + theme_bw()
+p <- ggplot(df, aes(x=date, y=prop)) + geom_point(color="#3d62a6") + geom_smooth(se=FALSE, span=1.5, color="black") + facet_wrap(.~func) + theme_bw()
+p <- ggplot(df[df$func %in% c("com", "ins", "goal", "recip", "poss"),] , aes(x=date, y=prop, group=func)) + geom_smooth(se=FALSE, span=1.5, color="black") + ylim(0,1) + theme_minimal()
+
+require(ggrepel)
+require(ggsci)
+p <- ggplot(df[df$func %in% c("com", "ins", "goal", "recip", "poss"),] , aes(x=date, y=prop, group=func)) + geom_smooth(se=FALSE, span=1.5, aes(color=func)) + ylim(0,1) + theme_bw() + theme(legend.position="none") + scale_color_npg() + ylab("Proportion of adpositional realization") + xlab("Year")
+labelsize <- 5.0
+p <- p + geom_text_repel(data=data.frame(date=1600, prop=0.99, func="com"), nudge_x=-5, nudge_y=-0.07, size=labelsize, aes(label=func))
+p <- p + geom_text_repel(data=data.frame(date=1680, prop=0.65, func="instrument"), nudge_x=-80, nudge_y=0.07, size=labelsize, aes(label=func))
+p <- p + geom_text_repel(data=data.frame(date=1700, prop=0.59, func="goal"), nudge_x=50, nudge_y=-0.08, size=labelsize, aes(label=func))
+p <- p + geom_text_repel(data=data.frame(date=1750, prop=0.31, func="recipient"), nudge_x=-50, nudge_y=0.05, size=labelsize, aes(label=func))
+p <- p + geom_text_repel(data=data.frame(date=1700, prop=0.14, func="poss"), nudge_x=50, nudge_y=-0.05, size=labelsize, aes(label=func))
+
+print(p)
+ggsave("test.pdf", height=4, width=6)
